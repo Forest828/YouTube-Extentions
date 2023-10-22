d355f2626ac6ec5240a431d5f02d4d3619ea0870
Forest828 <89926378+Forest828@users.noreply.github.com>
2023-10-22 23:10:48 +0200

    Update main_page_ad_remover.js

--- main_page_ad_remover.js
+++ main_page_ad_remover.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Removes the ad on the YouTube main page
 // @author       Forest828
-// @match        https://www.youtube.com/*
+// @match        https://www.youtube.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
